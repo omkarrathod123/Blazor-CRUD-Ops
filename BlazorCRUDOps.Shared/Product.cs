66517e413c0573629c1e9445b937ac70ff5e3682
omkarrathod123 <omkarrathod0705@gmail.com>
2026-03-10 14:36:38 +0530

    Shared Project Created!

---
+++ BlazorCRUDOps.Shared/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCRUDOps.Shared
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        [Required]
+        public string ProductDescription { get; set; }
+        [Required]
+        public string ProductCategory { get; set; }
+        [Required]
+        public double ProductPrice { get; set; }
+        [Required]
+        public double ProductQuantity { get; set; }
+    }
+}
