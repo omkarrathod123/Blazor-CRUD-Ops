919ec25dd4d55878649c30f9d8881db8aa5e3847
omkarrathod123 <omkarrathod0705@gmail.com>
2026-03-11 19:16:42 +0530

    CRUD Operation is created

---
+++ BlazorCRUDOps.Client/Pages/ProductEdit.razor.cs
@@ -0,0 +1,68 @@
+﻿using BlazorCRUDOps.Shared;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace BlazorCRUDOps.Client.Pages
+{
+    public partial class ProductEdit
+    {
+        [Parameter]
+        public int id { get; set; }
+        private bool ready;
+        private string? error;
+        private Product product;
+        private bool _initialized;
+        protected override async Task OnInitializedAsync()
+        {
+            if (_initialized) return;
+            _initialized = true;
+            if (id == 0)
+            {
+                product = new Product();
+            }
+            else
+            {
+                product = await Http.GetFromJsonAsync<Product>("Products/" + id);
+            }
+            ready = true;
+        }
+        private async Task HandleValidSubmit()
+        {
+            HttpResponseMessage responseMessage;
+            if (product.ProductId == 0)
+            {
+                responseMessage = await Http.PostAsJsonAsync("Products", product);
+            }
+            else
+            {
+                string requestUri = "Products/" + product.ProductId;
+                responseMessage = await Http.PutAsJsonAsync(requestUri, product);
+            }
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                NavigationManager.NavigateTo("productpage");
+            }
+            else
+            {
+                error = responseMessage.ReasonPhrase;
+            }
+        }
+        private async Task HandleRest()
+        {
+            NavigationManager.NavigateTo("productpage");
+        }
+        private async Task DeleteProduct()
+        {
+            string requestUri = "Products/" + product.ProductId;
+            var response = await Http.DeleteAsync(requestUri);
+            if (response.IsSuccessStatusCode)
+            {
+                NavigationManager.NavigateTo("productpage");
+            }
+            else
+            {
+                error = response.ReasonPhrase;
+            }
+        }
+    }
+}
