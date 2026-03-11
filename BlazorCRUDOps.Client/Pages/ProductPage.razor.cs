6e7b7727415cbbfb19b859feb6cc4ff4ca2379e3
omkarrathod123 <omkarrathod0705@gmail.com>
2026-03-11 19:41:32 +0530

    CRUD Operation Done!

---
+++ BlazorCRUDOps.Client/Pages/ProductPage.razor.cs
@@ -0,0 +1,22 @@
+﻿using BlazorCRUDOps.Shared;
+using System.Net.Http.Json;
+
+namespace BlazorCRUDOps.Client.Pages
+{
+    public partial class ProductPage
+    {
+        List<Product> productList = new List<Product>();
+        private bool _initialized;
+        protected override async Task OnInitializedAsync()
+        {
+            if (_initialized) return;
+            _initialized = true;
+            productList = await Http.GetFromJsonAsync<List<Product>>("Products");
+        }
+        private async Task HandleEdit(int id)
+        {
+            string requestUri = "product-edit/" + id.ToString();
+            navigationManager.NavigateTo(requestUri);
+        }
+    }
+}
